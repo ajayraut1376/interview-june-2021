b8696e0be329292bc23d7077e79de4ce22297eb6
Ajay Somnath Raut <64972970+ajayraut1376@users.noreply.github.com>
2021-06-22 12:15:03 +0530

    Update ConvertToBinary.java

--- src/main/java/exam/ConvertToBinary.java
+++ src/main/java/exam/ConvertToBinary.java
@@ -1,8 +1,23 @@
 package exam;
-
+import java.util.*;
+import java.util.Scanner;
 public class ConvertToBinary {
 
   public String toBinary(final int n) {
+    int num;
+      String str = "";
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please enter the a number : ");
+      num = sc.nextInt();
+      // convert int to binary java
+      while(num > 0)
+      {
+         int y = num % 2;
+         str = y + str;
+         num = num / 2;
+      }
+      System.out.println("The binary conversion is : " + str);
+      sc.close();
     //TODO write your code here to achieve the desired result as explained in Readme file for this problem.
     return "";
   }
